5f573bd42d639323c16a2686232dcbae7f7489bb
Yonatan Palagashvili <yonimotek@gmail.com>
2020-11-16 15:55:03 -0800

    set up testing file

---
+++ game_of_greed/game_logic.py
@@ -0,0 +1,22 @@
+import random
+from collections import Counter
+
+class GameLogic:
+
+    def calculate_score(self):
+        pass
+
+    @staticmethod
+    def roll_dice():
+        pass
+
+class Banker:
+
+    def shelf(self):
+        pass
+
+    def bank(self):
+        pass
+        
+    def clear_shelf(self):
+        pass
