3d9f2cc06a29628cc5923162c7647b78422aa48e
Mark Bell <skrambelled@gmail.com>
2020-11-17 15:52:14 -0800

    separated banker into its own module

---
+++ game_of_greed/banker.py
@@ -0,0 +1,18 @@
+class Banker:
+
+    def __init__(self):
+        self.balance = 0
+        self.shelved = 0
+
+    def shelf(self, shelved):
+        self.shelved += shelved
+        return self.shelved
+
+    def bank(self):
+        self.balance += self.shelved
+        self.clear_shelf()
+        return self.balance
+
+    def clear_shelf(self):
+        self.shelved = 0
+        return self.shelved
